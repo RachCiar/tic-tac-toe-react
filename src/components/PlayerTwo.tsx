097b1bb3a1c55911fb29423e34a813486c563d68
Rachel Ciarlante <rae.ciarlante@merck.com>
2023-03-13 14:59:23 -0400

    working on feature

--- src/components/PlayerTwo.tsx
+++ src/components/PlayerTwo.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/styles.module.scss'
 import DraggableGamePiece from './DraggableGamePiece'
 
 
 
-
 export const PlayerTwo = () => {
+  const [playerName, setPlayerName] = useState('Player One');
+  const [nameSubmitted, setNameSubmitted] = useState(false);
 
 
-  return (
-    <div className={styles.playerTwo}>
-      <h1 id={styles["playTwo"]}>Player Two</h1>
-      <form className={styles.formPlayerTwo}>
-        <p>Name:</p>
-        <input type="text" id={styles["nameP2"]} name="name2" />
-        <br /><br />
-        <input type="submit" className={styles.button} id={styles["submit2"]} value="Submit" />
-      </form>
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const name = (event.target as HTMLFormElement).elements.namedItem('nameP2') as HTMLInputElement;
+    if (name) {
+      setPlayerName(name.value);
+      setNameSubmitted(true);
+      hideFormForPlayer();
+    }
+  }
+  
+  function hideFormForPlayer() {
+    const formPlayerTwo = document.querySelector('.formPlayerTwo');
+    (formPlayerTwo as HTMLElement).style.display = 'none';
+    showGamePieces();
+  }
+
+  function showGamePieces() {
+    return(
       <div className={styles.pieces2}>
         <p id={styles["message2"]}>Click and drag your game piece to the board.</p>
         <DraggableGamePiece id={styles['p2Piece1']} piece="o" />
@@ -25,6 +35,24 @@ export const PlayerTwo = () => {
         <DraggableGamePiece id={styles['p2Piece4']} piece="o" />
         <DraggableGamePiece id={styles['p2Piece5']} piece="o" />
         <DraggableGamePiece id={styles['p2Piece6']} piece="o" />
+      </div>
+    )
+  }
+  
+  
+  return (
+    <div className={styles.playerTwo}>
+       <h1 id={styles['playTwo']}>{nameSubmitted ? playerName : 'Player Two'}</h1>
+      {!nameSubmitted && (
+        <form className={styles.formPlayerOne} onSubmit={handleSubmit}>
+          <p>Name:</p>
+          <input type="text" id={styles['nameP2']} name="nameP2" />
+          <br /><br />
+          <input type="submit" className={styles.button} id={styles['submit2']} value="Submit" />
+        </form>
+      )}
+      <div>
+      {nameSubmitted && showGamePieces()}
       </div>
     </div>
   )
